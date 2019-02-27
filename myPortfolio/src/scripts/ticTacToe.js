aad082607d994147090e6495566862a482746f3c
asa1411 <jungil_cho@yahoo.com>
2019-02-26 20:50:26 -0600

    tictactoe

---
+++ myPortfolio/src/scripts/ticTacToe.js
@@ -0,0 +1,10 @@
+// var gameMarker = "X";
+function changeMarkerToX() {
+  var gameMarker = "X";
+  console.log("The x button was clicked!");
+}
+function changeMarkerToO() {
+  var gameMarker = "Y";
+  console.log("The O button was clicked!");
+}
+function placeMark() {}
