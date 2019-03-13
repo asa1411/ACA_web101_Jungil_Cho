dc1366459589212d9273b0f3e99bce674da5a2dc
asa1411 <jungil_cho@yahoo.com>
2019-03-12 20:23:45 -0500

    comments on the tictactoe js code

--- myPortfolio/src/scripts/ticTacToe.js
+++ myPortfolio/src/scripts/ticTacToe.js
@@ -1,48 +1,52 @@
-// Sanity check - open your console to ensure it's working!
-console.log("Your javascript is working!");
-// First, we want to set a few Global Variables!
-// Set variable 'marker' to the letter 'O' initially
-// This means that every game will start with a player putting an 'X' down
-// If this doesn't make sense yet, check out the if statement
-let marker = "O";
-// The following two empty arrays will eventually hold the id's of the X and O occupied divs
-let checkXWinner = [];
-let checkOWinner = [];
-// We want to check if there has been a winner yet so that users can't click the board after someone has won
-let isWinner = false;
-// This function is called in the HTML onclick on each div
-// This function will not execute until a div is clicked
-// The 'id' is a placeholder value for the actual id of the div that is clicked on
-function place(id) {
-  // If isWinner is false, the program will continue to let users place X's and O's
-  if (!isWinner) {
-    // Sets variable divContent to check for any div without any content yet
-    var divContent = document.getElementById(id).innerHTML == "";
-    // Checks FIRST to see if the box has an X or an O already
-    // If the div clicked is empty, it will continue on to the inner if statement
-    if (divContent) {
-      // The if statement will determine whether an 'X' or 'O' will be placed
-      if (marker == "O") {
-        // Line 16 sets the inner HTML of whatever id of whatever div is clicked to 'X'
-        document.getElementById(id).innerHTML = "X";
-        // Set marker to 'X' once an 'X' is placed.
-        marker = "X";
-        checkXWinner.push(id);
-        if (checkXWinner[2]) {
-          winner(checkXWinner);
-        }
-      } else {
-        document.getElementById(id).innerHTML = "O";
-        // Set marker to 'O' once an 'O' is placed
-        marker = "O";
-        checkOWinner.push(id);
-        if (checkOWinner[2]) {
-          winner(checkOWinner);
-        }
+// Checking if it is working
+// console.log ("JavaScript is working now")
+// variable and its name with an array of 9 indexes "-" are given.
+var game = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+// A variable "turn" is set to "X."
+var turn = "X";
+// A variable "win" is set or assigned to an array of 8 nested arrays consisting of 3 indexes. Each nested array represents 3 boxes in a row, in a column or in a diagonal.
+var win = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+];
+// variable play is called here to relate to the function of onclick with a parameter boxNumber.
+var play = function(boxNumber) {
+  // A variable is defined in this function, and the variable box is assigned to the element with "box"Id and boxNumber paramenter.
+  var box = document.getElementById("box" + boxNumber);
+  // The condition states that the content of the box is not written X or O, then get X or O by turn variable.
+  if (box.textContent === "-") {
+    box.textContent = turn;
+    box.style.color = "#444";
+    // 	The game variable with index from 0 to 8 return O or X by turn until the 9 boxes are filled.
+    game[boxNumber] = turn;
+    // The for loop provides repetative execution of the block of statements 8 times because win.length value is 8.
+    for (var i = 0; i < win.length; i++) {
+      // 		 first square bracket calls the first index and second bracket calls the first number of the first index. If match1, match2 and match3 are consistently the same letter rendered by turn, a block of statements in the curly bracket will be executed.
+      const match1 = game[win[i][0]];
+      const match2 = game[win[i][1]];
+      const match3 = game[win[i][2]];
+      if (match1 === turn && match2 === turn && match3 === turn) {
+        // 			Use Box5 to declare that the symbol rendered by turn wins with style of the whole 9 box container.
+        var title = document.getElementById("box5");
+        var gameBoard = document.getElementById("container");
+        gameBoard.style.pointerEvents = "none";
+        gameBoard.style.backgroundColor = "goldenrod";
+        gameBoard.style.boxShadow = "0 0 10px 15px goldenrod";
+        title.textContent = turn + " WINS!";
       }
-    } else {
-      // Alerts the user if there's already an X or O in that spot
-      alert("You've already placed here!");
     }
+    // start to put X in the box as stated above and then O and then X.
+    if (turn === "X") {
+      turn = "O";
+    } else {
+      turn = "X";
+    }
+    console.log(game, turn);
   }
-}
+};
