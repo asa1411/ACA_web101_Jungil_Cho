fdd39548dcbbb5626249d125255587a9fbb9ba66
asa1411 <jungil_cho@yahoo.com>
2019-03-22 00:29:58 -0500

    final checkpoint3 in dashboard

--- myPortfolio/src/dashboard/script.js
+++ myPortfolio/src/dashboard/script.js
@@ -1,23 +1,24 @@
 onclick = "btnClicked(1)";
+// button click action is assigned to a function "btnClicked(1)."
 var score = 0;
+// variable score starts with 0 at the beginning.
 function btnClicked(number) {
   score = score + number;
 
   document.getElementById("score").innerHTML = score;
 }
+// The function btnClicked with parameter or argument number is made of two statements, and the number is recorded in the element "score."
 onclick = "buySmallBaker()";
 var baker1 = 0;
 function buySmallBaker() {
-  // var smallBakerCost = Math.floor(10 * Math.pow(1.1, baker1));
-  var smallBakerCost;
   if (score >= 10) {
     baker1 = baker1 + 1;
     score = score - 10;
   }
-
+  // Another function buySmallBaker is made with if condition statement. Baker1 is bought with 10 scores. Score records the number of score minus 10.
   document.getElementById("smallBakerNumber").innerHTML =
     "popcorn maker(s): " + baker1;
-  // document.getElementById("score").innerHTML = score1;
+  // The number of baker1 purchased is recorded in the element "smallBakerNumber."
 }
 onclick = "sellSmallBaker()";
 function sellSmallBaker() {
@@ -39,7 +40,7 @@ function buyMediumBaker() {
   document.getElementById("mediumBakerNumber").innerHTML =
     "popcorn maker(s): " + baker2;
 }
-// var nextBakerCost = Math.floor(10 * Math.pow(1.1, baker1));
+// The above and below functions follow the same pattern as the first function.
 
 onclick = "sellMediumBaker()";
 function sellMediumBaker() {
@@ -57,3 +58,4 @@ window.setInterval(function() {
 window.setInterval(function() {
   btnClicked(baker2);
 }, 500);
+// These setInterval method of the two above functions returns the btnClicked with baker1 and baker2 every 1 second and every 0.5 second respectively.
